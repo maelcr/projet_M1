9bca740f275e5b770ada69ff13d50ee1ee3f16d1
maelcr <mael.creismeas@isen-ouest.yncrea.fr>
2024-02-18 16:27:37 +0100

    update

--- ia_yolo_detection/test_yolo_V8.py
+++ ia_yolo_detection/test_yolo_V8.py
@@ -1,4 +1,4 @@
-
+"""
 #entrainement : Entraîne YOLOv8n sur l'ensemble de données COCO128 pendant 100 époques à une taille d'image de 640. Pour une liste complète des arguments disponibles, voir la page Configuration.
 from ultralytics import YOLO
 
@@ -9,6 +9,7 @@ model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer 
 
 # Train the model
 results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
+"""
 
 """
 #validationb : Valide la précision du modèle YOLOv8n entraîné sur le jeu de données COCO128. Aucun argument n'a besoin d'être passé en tant que model conserve sa formation data et les arguments en tant qu'attributs du modèle.
@@ -25,7 +26,7 @@ metrics.box.map50  # map50
 metrics.box.map75  # map75
 metrics.box.maps   # a list contains map50-95 of each category
 """
-"""
+
 #prédiction : Utilise un modèle YOLOv8n entraîné pour faire des prédictions sur les images.
 from ultralytics import YOLO
 
@@ -35,7 +36,7 @@ model = YOLO('path/to/best.pt')  # load a custom model
 
 # Predict with the model
 results = model('https://ultralytics.com/images/bus.jpg')  # predict on an image
-"""
+
 
 """
 #exporte : Exporte un modèle YOLOv8n vers un format différent comme ONNX, CoreML, etc.
