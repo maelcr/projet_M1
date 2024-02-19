dd9301246a4b81b26d70e3a043bef1d67f43f6aa
maelcr <mael.creismeas@isen-ouest.yncrea.fr>
2024-02-19 22:31:51 +0100

    update

--- ia_yolo_detection/test_2_yolo_V8.py
+++ ia_yolo_detection/test_2_yolo_V8.py
@@ -1,7 +1,37 @@
 from ultralytics import YOLO
-
+ 
+# Load the model.
 model = YOLO('yolov8n.pt')
-model.predict(
-   source='https://media.roboflow.com/notebooks/examples/dog.jpeg',
-   conf=0.25
-)
+ 
+# Training.
+results = model.train(
+   #data='C:/Users/Utilisateur/Documents/GitHub/projet_M1/datasets/ia_yolo_detection/project2.v3i.yolov8/data.yaml',
+   data='dataset_poussin/data.yaml',
+   imgsz=640,
+   epochs=5,
+   batch=8,
+   name='yolov8n_custom')
+
+print("finis")
+"""from ultralytics import YOLO
+
+# Load a model
+model = YOLO('ia_yolo_detection/project2.v3i.yolov8/yolov8n.yaml')  # build a new model from YAML
+
+# Train the model
+results = model.train(data='coco128.yaml', epochs=25, imgsz=640)
+"""
+
+"""
+from ultralytics import YOLO
+ 
+# Load the model.
+model = YOLO('yolov8n.pt')
+ 
+# Training.
+results = model.train(
+   data='custom_data.yaml',
+   imgsz=640,
+   epochs=10,
+   batch=8,
+   name='yolov8n_custom'"""
