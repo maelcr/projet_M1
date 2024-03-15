21d617c60a3060777d75198a6e06de7aa54aa4e6
creismeas Maël <mael.creismeas@outlook.fr>
2024-03-15 11:27:55 +0100

    Create IA_detect_poussin.py

---
+++ APP_projet_python_v2/IA_detect_poussin.py
@@ -0,0 +1,17 @@
+import os
+import ultralytics
+from ultralytics import YOLO
+
+
+
+
+class IA_detect_poussin():
+
+    def __init__(self):
+        self.HOME = os.getcwd()
+        self.model=YOLO(f'{HOME}/../yolov8n.pt')
+    
+    def predict_image(self, file_name):
+        script_dir = os.path.dirname(__file__)
+        abs_file_path = os.path.join(script_dir, file_name)
+        self.model.predict(abs_file_path, save=True, imgsz=800, conf=0.25)
